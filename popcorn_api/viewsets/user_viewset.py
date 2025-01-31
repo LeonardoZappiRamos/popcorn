fdb61ccbd27a25a06820694f058926016ea0b42f
Leonardo Zappi <leo.zappi@hotmail.com>
2025-01-31 22:37:41 +0000

    Fixing permition

--- popcorn_api/viewsets/user_viewset.py
+++ popcorn_api/viewsets/user_viewset.py
@@ -1,5 +1,5 @@
 from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAdminUser
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAdminUser, AllowAny
 from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
 from django.contrib.auth.models import User
@@ -9,7 +9,9 @@ from popcorn_api.serializers import UserSerializer
 class UserViewSet(viewsets.ViewSet):
     def get_permissions(self):
         if self.action == "list":
-            permission_classes = [IsAuthenticatedOrReadOnly]
+            permission_classes = [IsAuthenticated]
+        else if self.action == "list":
+            permission_classes = [AllowAny]
         else:
             permission_classes = [IsAdminUser]
         return [permission() for permission in permission_classes]
